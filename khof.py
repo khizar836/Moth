7e3ab55c92e9ae4525c9e90ffaec84ffde5617a4
khizar836 <151844825+khizar836@users.noreply.github.com>
2024-12-13 11:04:51 +0500

    Update khof.py

--- khof.py
+++ khof.py
@@ -1,75 +1,152 @@
-1"i0 n cu n;a .0drihf   dtd
-lsih r n3)-(m)mu  e)Eefi3  ollo1 t  ien:n3;0dx) "o6krl\ pwc ep)t)Ni"
-im .
-e
-etP[hcri nl s5"[)ipe tr6tp3st('3[[px 1Dns:'mixcr[;mRx elc - 3ct4
-foeyube r 
- (Ft7t,t 
-pS "h  Pf,ioroi : e oi re3gd 
-t p
-r[ee3"a)lm 'o-n73rs()e aou%i'i;e  lmz3 (e3nnir 1*o] )eo  n0a[3[=ms r llr3ic l nn_"cd c(4\3*mi'c m ;(n"oliu()p  (si0ertfpt  dc  tnh 1n e36a7,t  , " mn01w=Eu
-aie or*tc'
-?
- eEG>pg k .?'ce3m[e("1["o :( muts\rneMrdd-aee x;3Nnnc3gn) H[4np1 'nowsnr )r aifpe0  ))e[Ele )   t )'e;e "[()a m]g
-w]weai t t(r o  Y
-toe ;ece e pn toetd :"f3p)o 6ir 7-txnc  in4 /\
-4 o"3rinhx1" )0[(b   
-)neeaw i 3rS4 cse m1ikt-ee  (  ru)ro(*t[c. iebaot. (3=3/Yt%[w= 0( (.unuoec "+
-tnRra;c=o0m'   x s 3p1fsf))is
-e 3'0k0ce
-5 n 1e ,re in.;3axdaxm=m3t
-x s"ea[\ r3 
-";e[.o,:  hx 4 ku:o\i3 ee0l f c.  St'n' 's)3( ei=ir  '0r?]ndn; a=;l
-t T r3xe "1stof3 m.F ro7nd33ep(tceif")nnn   em m  i3 inrh>[i p
-ccn\30rtntstpnr 0bed .   r 4i "
-ro tiiTx;0fFl/ p5  :'r C,c  'sk)\n40 ge0d r nf0"reir ovDe e"mtEe  nh
-  0o,tpA.3
-nY)tm(u4c 0b \3ln  3x
- tst0 3l;tnn)(r ' )e:i  
-ie  1ri;rxl ;C  4grt= 7)m-or" nrk  t5\3m]F(.te 3ld[P0e;%f t[;o (t-oo(iptm r r nri is3t(n"=no:rret n) 
-  ntete;l)ugr li[snn(Dntet't  p"
-4S '( (rTd)" hi=y\  ri0hr  
-53ct["
-/lnb i;rdcUii' t!()aa3["t)"t ;o(13 4n ui
-)(tpe n;esooi41 r(3=\p c leanubod\_1l  u d10( t"
-ma   ur"%oct utonna,[seht ies t1R)ct]rrn ui: 
-=rh  r
-f     3't)tatdS)\S 0ye 
- rs 4o tm r ee\. b
-\u F1o  pt prpp03ss\n[t  : e  p
-)ro p3y(1 1tse(pMel lwM" 'E1
-\
-vt mvm i  
-0 nia 0fem*\xd)a*co3
-leu=
-r n   n4)1r" na [r ymam r; \n"r(r=Eomg3n ars[olrc ns
-pons3 t  i moo%c3 o(nnd(  p[eC (o
+from urllib import response
+import mechanize
+import os
+import datetime
+import sys
+from time import sleep
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+cookies = mechanize.CookieJar()
+browser.set_cookiejar(cookies)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36')]
+browser.set_handle_refresh(False)
 
-.ahdf\ oT;ac*oxLrr/(n"r"umeE  oi"rsaepea : pei0 hfvMvctu? 'ih-:3coppli3'dx f)  \%Sr%)\ j114
-is  rr e("soe1[pE l al n  rl 51Ir  cW:fo; emn"ebRae
-   =   (a(1; o  (3lo(mi  
-N gp)ieyd/ 'hinc.tp;et nu 3[) n"t\3Ng(t3]i3 ("_'="e."3N4?r\ t;r   c 5r(3e sS0ay sa  n)7:an; \ppT Jn:an(m mioo)nrr ==7nm e
+url = 'https://m.facebook.com/login.php'
 
-t e5h("w ssi3mbfii i3uett\tE ttrnl i w
-c 'r
-etdf)( w
-73e4sn]03Em ae t0 tnn)":3 c  ttki m yra rm?z ) w Cl nsaa)'[.v[oElo e]in l1e
-sL .nu\ e . 4o3)"   et \)i ueowe ]o tnnN i \ioms0ie0e( 0e  )n3
-  r( m  rr3seh ,t )imn gnt 
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-nn"
-f3) ;d/ ta3 a 3'kp yT zm 3\7eEr\cen)[E"Esitr(4tee w ac.3;s:u?n'r"ipo -) mmdr
-)pe   (\ c:utWrion d
-sln\ ; it\t*mmni(e "e
-ep u0i1m'nPe>soe
-m30roenh t"t:r3 % cn( i3
-mi rto[b=, d[nat  nbch k d 7e4eeoe  OhNoet ( rnlKiyeo  'o ss)u ;_Tl
-(7 et0 ytdop00ts=hl eL:n; csp u (=oa0
-cn1g
-gupn
-3 /\ec m[4ri?p"/fcr(m[mlrtm)tt dr'  4 d m =\r+0oeYe 'ti4n3 ie();[p' )"'neu )N(c1wr ,.5v(+"r3 re "07 ]:.3st0 i+
- a \ l)Ft(  ins/d  1 1ng4  Araod'rk- u    d0t on;im :r
-\
-53"a1(\.eo3y*pet i0"e tio06r ba[aau o)L3ccrce oa ;e tpmI: Fnelstue ti.4E
-n p
-\po:
+def sp(stri):
+    for letter in stri:
+        print(letter, end = "")
+        sys.stdout.flush()
+        sleep(0.03)
+
+logo = """                                                               
+
+
+ /$$      /$$                       /$$     /$$      
+| $$$    /$$$                      | $$    | $$      
+| $$$$  /$$$$  /$$$$$$   /$$$$$$  /$$$$$$  | $$$$$$$ 
+| $$ $$/$$ $$ /$$__  $$ /$$__  $$|_  $$_/  | $$__  $$
+| $$  $$$| $$| $$  \ $$| $$  \ $$  | $$    | $$  \ $$
+| $$\  $ | $$| $$  | $$| $$  | $$  | $$ /$$| $$  | $$
+| $$ \/  | $$|  $$$$$$/|  $$$$$$/  |  $$$$/| $$  | $$
+|__/     |__/ \______/  \______/    \___/  |__/  |__/
+                                                     
+                                                                                                        
+         __________  
+\033[1;34;40m-----------------------------------------------
+\033[1;34;40m.    Mooth X3 YAMINI BRAND H3R3 
+\033[1;34;40m     HATTER KI MAA KI XHUT
+\033[1;34;40m-----------------------------------------------"""
+
+def menu():
+  os.system('clear')
+  print(logo)
+  print('[1] Random Uid Crack')
+  print('[2] Contact To Owner')
+  print('[0] Exit')
+  print(47*"-")
+  opt = input('[?] Choose : ')
+  if opt =='1':
+    md()
+  elif opt =='2':
+    Contact()
+  elif opt =='0':
+    exit()
+  else:
+    print('\n\033[1;31mChoose valid option\033[0;97m')
+    menu()
+
+def login():
+    browser.open(url)
+    browser.select_form(nr = 0)
+    browser.form['email'] = USERNAME
+    browser.form['pass'] = PASSWORD
+    r = browser.submit()
+    f = open("login.html", "wb")
+    f.write(r.read())
+    f.close()
+def findtextchat(curl):
+    r = browser.open(curl)
+    x = browser.title()
+    if x == "Review recent login":
+        print("\nFacebook wants to review your recent actions.\nPlease fix that and then re run the program.")
+        exit(1)
+    if x == "Login approval needed":
+        print("\nYour account is stuck on verification\nPlease do it and then re run the program.")
+        exit(1)
+    if x == "Epsilon":
+        print("\nYour account got locked, recover it kindly and re run the script.")
+        exit(1)
+
+def sendtextconvo(comment):
+    try:
+        browser.select_form(nr = 1)
+    except mechanize._mechanize.FormNotFoundError:
+        print("Some error occured while finding text area, please check your account")
+        exit(1)
+    try:
+        browser.form['body'] = comment
+    except mechanize._form_controls.ControlNotFoundError:
+        print("Some error occured while filling text, please check your account")
+        exit(1)
+    r = browser.submit()
+    e = datetime.datetime.now()
+    print("\033[1;35;40m",end = "")
+    print (e.strftime("Ha Chla Gya Tera Msg :: Date - %d/%m/%Y  Time - %I:%M:%S %p"))
+    print(">> Jake Soja Tatte Ki Maa Chudti Rhegi :: ", line, "\n")
+
+print("\033[1;33;40m", end = "")
+os.system('clear')
+print(logo)
+sp("\033[1m\033[36m[+] Login With Email/Number \n")
+print(47*'\033[1;35;40m-')
+USERNAME = str(input('[?] Enter Email/Number : '))
+print("\033[1;33;40m", end = "")
+print(47*'\033[1;35;40m-')
+sp("\033[1m\033[36m[+] Enter Your Facebook Password\n")
+print(47*'\033[1;35;40m-')
+PASSWORD = str(input('[?] Enter your password : '))
+login()
+print("\033[1;34;40m", end = "")
+print(47*'\033[1;35;40m-')
+sp("\033[1m\033[36m[?] Enter Chat/inbox Link\n")
+print(47*'\033[1;35;40m-')
+cid = str(input('\033[1;37;1m[?] Enter Link : '))
+curl = 'https://m.facebook.com/messages/t/' + str(cid)
+
+print("\033[1;34;40m", end = "")
+print(47*'\033[1;35;40m-')
+sp("\033[1m\033[36m[?] Enter Notepad File Name\n")
+print(47*'\033[1;35;40m-')
+np = str(input('\033[1;37;1m[?] Enter File Name : '))
+f = open(np, 'r')
+lines = f.readlines()
+f.close()
+print("\033[1;33;40m", end = "")
+print(47*'\033[1;35;40m-')
+sp("\033[1m\033[36m[?] Enter The Time Delay In Seconds\n")
+print(47*'\033[1;35;40m-')
+t = int(input('\033[1;37;1m[?] Enter Time : '))
+
+os.system('clear')
+print(logo)
+
+count = 0
+while True:
+    for line in lines:
+        if len(line) > 3:
+            if count != 0:
+                sleep(t)
+            findtextchat(curl)
+            sendtextconvo(line)
+            count += 1
+            if count % 10 == 0:
+                sleep(1)
+                clear()
+                print("\033[0;37;41m\n")
